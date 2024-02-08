3e107091c844c66a1aa013c2f2ed7f13910e914f
Harry <harrypham_@hotmail.com>
2024-02-08 22:55:30 +1100

    create frames/s

--- src/Components/MainPage/MainPage.tsx
+++ src/Components/MainPage/MainPage.tsx
@@ -1,23 +1,72 @@
 'use client';
 
 import { Shrimp } from '../Shrimp/Shrimp';
+import map from '../../Assets/Background/PelletTown.png';
 import styles from './MainPage.module.css';
+import { useState, useEffect } from 'react';
+import Image from 'next/image';
+
+type spritePosition = {
+  spriteX: number;
+  spriteY: number;
+};
+
+type keysInput = {
+  left: boolean;
+  up: boolean;
+  right: boolean;
+  down: boolean;
+};
 
 const MainPage: React.FunctionComponent = () => {
-  const arrowKeys = (e: React.KeyboardEvent<HTMLElement>) => {
+  const [input, setInput] = useState<keysInput>({
+    left: false,
+    up: false,
+    right: false,
+    down: false,
+  });
+
+  const [pos, setPos] = useState<spritePosition>({
+    spriteX: 0,
+    spriteY: 0,
+  });
+
+  const inputKey = (e: React.KeyboardEvent<HTMLElement>) => {
     if (e.keyCode === 37) {
-      console.log('left');
+      setInput({ ...input, left: true });
     } else if (e.keyCode === 38) {
-      console.log('up');
+      setInput({ ...input, up: true });
     } else if (e.keyCode === 39) {
-      console.log('right');
+      setInput({ ...input, right: true });
     } else if (e.keyCode === 40) {
-      console.log('down');
+      setInput({ ...input, down: true });
     }
   };
+
+  const offPutKey = (e: React.KeyboardEvent<HTMLElement>) => {
+    event.preventDefault();
+    if (e.keyCode === 37) {
+      setInput({ ...input, left: false });
+    } else if (e.keyCode === 38) {
+      setInput({ ...input, up: false });
+    } else if (e.keyCode === 39) {
+      setInput({ ...input, right: false });
+    } else if (e.keyCode === 40) {
+      setInput({ ...input, down: false });
+    }
+  };
+
   return (
-    <main className={styles.main} tabIndex={0} onKeyDown={arrowKeys}>
-      <Shrimp />
+    <main
+      className={styles.main}
+      tabIndex={0}
+      onKeyDown={inputKey}
+      onKeyUp={offPutKey}
+    >
+      <section>
+        {/* <Image src={map} alt="Map" className={styles.bg} /> */}
+        <Shrimp pos={pos} setPos={setPos} input={input} setInput={setInput} />
+      </section>
     </main>
   );
 };
--- src/Components/Shrimp/Shrimp.tsx
+++ src/Components/Shrimp/Shrimp.tsx
@@ -1,10 +1,51 @@
-import { useMemo } from 'react';
+import { useMemo, useState, useEffect } from 'react';
 
 import { Character, stances } from '@/Assets/index';
 
 import styles from './Shrimp.module.css';
 
-const Shrimp: React.FunctionComponent = () => {
+const updateVar = (vars, value): any => {
+  if (typeof document === 'undefined') return;
+  return document.documentElement.style.setProperty(vars, value);
+};
+
+const position = () => {
+  if (typeof document === 'undefined') return;
+  // console.log(document.getElementById('shrimp')?.getBoundingClientRect());
+  return document.getElementById('shrimp')?.getBoundingClientRect();
+};
+
+const Shrimp: React.FunctionComponent = ({ pos, setPos, input, setInput }) => {
+  const [frameCount, setFrameCount] = useState<number>(0);
+
+  const frame = () => {
+    animate();
+    updateVar('--shrimpX', pos.spriteX + 'px');
+    updateVar('--shrimpY', pos.spriteY + 'px');
+    setFrameCount((prev) => prev + 1);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => frame(), 16);
+    return () => clearInterval(interval);
+  }, [frameCount]);
+
+  const animate = () => {
+    if (Object.keys(input).every((boo) => !input[boo])) return;
+    if (input.left) {
+      setPos({ ...pos, spriteX: pos.spriteX - 5 });
+    }
+    if (input.up) {
+      setPos({ ...pos, spriteY: pos.spriteY - 5 });
+    }
+    if (input.right) {
+      setPos({ ...pos, spriteX: pos.spriteX + 5 });
+    }
+    if (input.down) {
+      setPos({ ...pos, spriteY: pos.spriteY + 5 });
+    }
+  };
+
   const character = useMemo(
     () => (
       <Character
@@ -18,7 +59,7 @@ const Shrimp: React.FunctionComponent = () => {
     ),
     [],
   );
-  return <div>{character}</div>;
+  return <>{character}</>;
 };
 
 export { Shrimp };
